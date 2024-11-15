5f856752d51a23e69a4f7d6fcf0b067c4ce20dd0
WuMaxtw <WuMax13@gmail.com>
2024-11-15 09:22:06 +0800

    test

--- testSpace/main.c
+++ testSpace/main.c
@@ -1,19 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-int glob = 6;
 int main(void){
-    int var;
-    pid_t pid;
-    var = 88;
-    if ((pid = vfork()) < 0) {
-        printf("vfork error");
-        exit(-1);
-    } else if (pid == 0) {
-        var++;
-        glob++;
-        _exit(0);
-    }
-    printf("pid=%d, glob=%d, var=%d\n", getpid(), glob, var);
-    exit(0);
+	pause();
+
+	return 0;
 }
